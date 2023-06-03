f555496e8a7926d7aa3551303dbb4cc95eea9846
kamagata-ko <eighth.9512@gmail.com>
2023-06-03 20:32:22 +0900

    classを作成

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ class TaskManager {
   constructor() {
     this.tasks = [];
   }
+  addTask(task) {
+    this.tasks.push(task);
+  }
+  deleteTask(taskId) {
+    this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+  showTasks() {
+    this.tasks.forEach((task) => {
+      console.log(`[${task.id}] ${task.title} - ${task.description}`);
+    });
+  }
+}
+
+class Task {
+  constructor(id, title, description) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+  }
 }
