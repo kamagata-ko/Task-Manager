06c149c56419f79e72cc179500e1bdbfb3046e47
kamagata-ko <eighth.9512@gmail.com>
2023-06-04 11:56:43 +0900

    インスタンスを作成

--- taskManager.js
+++ taskManager.js
@@ -22,3 +22,16 @@ class Task {
     this.description = description;
   }
 }
+
+const taskManager = new TaskManager();
+
+const task1 = new Task(1, "タスク１", "スーパーへ買い物に行く");
+const task2 = new Task(2, "タスク２", "プログラミングを勉強する");
+taskManager.addTask(task1);
+taskManager.addTask(task2);
+
+// taskManager.showTasks();
+
+// taskManager.deleteTask(1);
+
+// taskManager.showTasks();
